43e0e651fe4bb6236e6e19cab04b4bc5aac84def
yamawo <y7a3m1asaki@gmail.com>
2019-09-12 19:24:51 +0900

    add group name,group member

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
 
     def update
         if current_user.update(user_params)
-            redirect_tp root_path
+            redirect_to root_path
         else
             render :edit
         end
     end
+    
 
     private
 
