1ba6cd43fb46e0e213821f42de77447845110ca4
yamawo <y7a3m1asaki@gmail.com>
2019-09-23 22:22:42 +0900

    create time_formats.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ChatSpace
       g.test_framework false
     end
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
   end
 end
